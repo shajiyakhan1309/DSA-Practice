782187938009543e1fe311258babce14a6d3ff45
shajiyakhan1309 <shajiyakhan1309@gmail.com>
2024-08-08 16:25:48 +0530

    Start

--- TreeNode.java
+++ TreeNode.java
@@ -3,44 +3,46 @@ public class TreeNode {
         TreeNode left;
         TreeNode right;
         TreeNode(int x) { val = x; }
-     }
-     class Solution {
-        
+    }
+    
+    class Solution {
         public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-            
+            // Implement the logic for finding the lowest common ancestor here
         }
     }
     
-    
-    static class BinaryTree{
+    static class BinaryTree {
         static int idx = -1;
-        public static Node buildTree(int nodes[]){
-                idx++;
-            if(nodes[idx]==-1){
+        public static TreeNode buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
                 return null;
             }
-            Node newNode = new TreeNode(nodes[idx]);
+            TreeNode newNode = new TreeNode(nodes[idx]);
             newNode.left = buildTree(nodes);
             newNode.right = buildTree(nodes);
             return newNode;
         }
-        public static void postOrder(Node root){
-                if(root == null){
-                    System.out.print(-1+" ");
-                    return;
-                }
-                postOrder(root.left);
-                postOrder(root.right);
-                System.out.print(root.data+" ");
+    
+        public static void postOrder(TreeNode root) {
+            if (root == null) {
+                System.out.print(-1 + " ");
+                return;
             }
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.val + " ");
+        }
     }
-    public static void main(String args[]){
-       int root[] = {6,2,8,0,4,7,9,null,null,3,5};
+    
+    public static void main(String args[]) {
+        int nodes[] = {6, 2, 8, 0, 4, 7, 9, -1, -1, 3, 5};
         int p = 2;
         int q = 8;
     
         BinaryTree tree = new BinaryTree();
-        Node root = tree.lowestCommonAncestor(nodes);
-        System.out.println(root.data);
-        postOrder(root);
+        TreeNode root = tree.buildTree(nodes);
+        System.out.println(root.val);
+        BinaryTree.postOrder(root);
     }
+    
