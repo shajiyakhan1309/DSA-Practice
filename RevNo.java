9ab713fc3adb11b044b81802ee9f161fec27a9ce
shajiyakhan1309 <shajiyakhan1309@gmail.com>
2024-08-29 15:02:11 +0530

    Practice Q

--- RevNo.java
+++ RevNo.java
@@ -1,30 +1,37 @@
 import java.util.ArrayList;
+
 class RevNo {
-    public static void reverseSubArray(ArrayList<Integer> arr, int l,
-        int r) {
-                for(int i=0;i<arr.size();i++){
-                    if(i == r){
-                        arr.reversed();
-                    }
-                }
-                       
-        }                                   
-        
-        public static void main(String args[]){
-            ArrayList<Integer> arr = new ArrayList<Integer>();
-            arr.add(1);
-            arr.add(2);
-            arr.add(3);
-            arr.add(4);
-            arr.add(5);
-            arr.add(6);
-            arr.add(7);
-            
-            System.out.println(arr);
-            
-            int l = 2;
-            int r = 4;
-            
-            reverseSubArray(arr, l, r);
+    public static void reverseSubArray(ArrayList<Integer> arr, int l, int r) {
+        int start = arr.indexOf(l);
+        int end = arr.indexOf(r);
+
+        while(start<end){
+            int temp = arr.get(start);
+            arr.set(start, arr.get(end));
+            arr.set(end,temp);
+            start++;
+            end--;
         }
+        
+    }                                   
+        
+    public static void main(String args[]) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+        arr.add(5);
+        arr.add(6);
+        arr.add(7);
+        
+        System.out.println("Original array: " + arr);
+        
+        int l = 2;
+        int r = 4;
+        
+        reverseSubArray(arr, l, r);
+        
+        System.out.println("Array after reversing subarray: " + arr);
     }
+}
